96ae3440b5d4544ad8f92dbfadfa6a7c020422e1
benoitgallet <benoit.gallet@live.fr>
2018-03-01 20:14:12 +0100

    ride

--- src/hashcode/Ride.java
+++ src/hashcode/Ride.java
@@ -8,4 +8,83 @@ public class Ride {
     private int start;
     private int finish;
     private Vehicle vehicle;
+
+    public Ride(){
+
+    }
+
+    public Ride(int startR, int startC, int stopR, int stopC, int start, int finish) {
+        this.startR = startR;
+        this.startC = startC;
+        this.stopR = stopR;
+        this.stopC = stopC;
+        this.start = start;
+        this.finish = finish;
+    }
+
+    public Ride(int startR, int startC, int stopR, int stopC, int start, int finish, Vehicle vehicle) {
+        this.startR = startR;
+        this.startC = startC;
+        this.stopR = stopR;
+        this.stopC = stopC;
+        this.start = start;
+        this.finish = finish;
+        this.vehicle = vehicle;
+    }
+
+    public int getStartR() {
+        return startR;
+    }
+
+    public void setStartR(int startR) {
+        this.startR = startR;
+    }
+
+    public int getStartC() {
+        return startC;
+    }
+
+    public void setStartC(int startC) {
+        this.startC = startC;
+    }
+
+    public int getStopR() {
+        return stopR;
+    }
+
+    public void setStopR(int stopR) {
+        this.stopR = stopR;
+    }
+
+    public int getStopC() {
+        return stopC;
+    }
+
+    public void setStopC(int stopC) {
+        this.stopC = stopC;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getFinish() {
+        return finish;
+    }
+
+    public void setFinish(int finish) {
+        this.finish = finish;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
 }
